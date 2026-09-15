f3edf17a9fd6f23c148b65504512b249692f99e9
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2026-09-15 11:10:59 -0300

    feat: rota para buscar todos os produtos (sem paginamento)

--- src/main/java/com/gabrielsilva/dscommerce/services/ProductServices.java
+++ src/main/java/com/gabrielsilva/dscommerce/services/ProductServices.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +21,11 @@ public class ProductServices {
         Optional<Product> result = repository.findById(id);
         Product product = result.get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductDTO> findAll(){
+        List<Product> result = repository.findAll();
+        return result.stream().map(ProductDTO::new).toList();
     }
 }
