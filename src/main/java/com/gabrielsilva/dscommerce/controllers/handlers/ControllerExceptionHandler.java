eebc2f117e5762c34eea5e0ae6e44e976b6a8915
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2026-09-16 08:07:34 -0300

    feat: adiciona validacao de dados e excecoes customizadas

--- src/main/java/com/gabrielsilva/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/gabrielsilva/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.gabrielsilva.dscommerce.controllers.handlers;
 
 import com.gabrielsilva.dscommerce.dto.CustomError;
+import com.gabrielsilva.dscommerce.dto.ValidationError;
 import com.gabrielsilva.dscommerce.services.exceptions.DatabaseException;
 import com.gabrielsilva.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,6 +28,16 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> resourceNotFoundException(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), request.getRequestURI(), e.getMessage(), status.value());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), request.getRequestURI(), "Dados Inválidos", status.value());
+        for (FieldError f: e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
---
+++ src/main/java/com/gabrielsilva/dscommerce/dto/ValidationError.java
@@ -0,0 +1,24 @@
+package com.gabrielsilva.dscommerce.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError {
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+
+    public ValidationError(Instant timestamp, String path, String error, Integer status) {
+        super(timestamp, path, error, status);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+
+}
